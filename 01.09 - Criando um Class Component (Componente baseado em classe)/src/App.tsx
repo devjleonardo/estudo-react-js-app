a16d8741c26dcac6de0f6f00815aacc2e3d93aea
Leonardo <leonardo_cordeirobahia@hotmail.com>
2024-05-15 21:49:56 -0300

    Criando um Class Component (Componente baseado em classe)

---
+++ 01.09 - Criando um Class Component (Componente baseado em classe)/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+import Post from "./components/Post";
+import Card from "./components/Card";
+
+function App() {
+  const [post] = useState({
+    title: "Título maneiro",
+    content: "Lorem Ipsum dolor sit ament",
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Card align="left" title="Card">
+          <Post post={post} totalComments={12} />
+        </Card>
+      </header>
+    </div>
+  );
+}
+
+export default App;
