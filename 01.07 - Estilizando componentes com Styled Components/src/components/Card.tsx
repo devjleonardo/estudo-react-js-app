5142600bf6f81ec915c112f89a69165ff6e8ca0a
Leonardo <leonardo_cordeirobahia@hotmail.com>
2024-05-15 21:46:31 -0300

    Estilizando componentes com Styled Components

---
+++ 01.07 - Estilizando componentes com Styled Components/src/components/Card.tsx
@@ -0,0 +1,16 @@
+import { PropsWithChildren } from "react";
+import * as C from "./Card.style";
+
+type CardProps = PropsWithChildren<{
+  title: string;
+  align?: "center" | "left" | "right";
+}>;
+
+export default function Card(props: CardProps) {
+  return (
+    <C.Wrapper align={props.align || "left"}>
+      <C.Title>{props.title}</C.Title>
+      {props.children}
+    </C.Wrapper>
+  );
+}
